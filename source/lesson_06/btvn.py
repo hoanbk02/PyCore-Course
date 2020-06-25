2ae2c86a1e9fdf1f1604433e211caa3aa9b67b76
Hoan PP <hoan.pp@teko.vn>
2020-06-25 20:36:57 +0700

    Lesson 06

---
+++ source/lesson_06/btvn.py
@@ -0,0 +1,38 @@
+""" BÀI TẬP VỀ NHÀ BUỔI 06 - DICTIONARY:
+
+Bài 00: Viết chương trình tính tích các phần tử trong một dict
+
+Bài 01: Viết chương trình tìm giá trị lớn nhất và giá trị nhỏ nhất của trường value trong một dict
+
+Bài 02: Viết chương trình sắp xếp các phần tử của dict theo chiều tăng dần của key
+
+Bài 03: Viết chương trình lấy các các giá trị không trùng lặp từ dict
+
+Bài 04: Viết chương trình tìm ra 3 phần tử có key lớn nhất trong dict
+
+Bài 05: Viết chương trình tạo ra dict với lớn hơn 3 phần tử, value của mỗi phần tử là một list có lớn hơn 5 phần tử.
+        Truy cập và in ra phần tử thứ 5 trong phần value của mỗi phần tử trong dict
+
+Bài 06: Viết chương trình lấy ra các phần tử key-value xuất hiện trong cả 2 dict
+
+Bài 07: Viết chương trình tạo dict mới bằng cách trích xuất dữ liệu từ dict ban đầu theo tập các key cho trước
+Ví dụ:
+    dict ban đầu: sampleDict = {"name": "Kelly", "age":25, "salary": 8000, "city": "New york"}
+    keys = ["name", "salary"]
+    Output: {'name': 'Kelly', 'salary': 8000}
+
+Bài 08: Viết chương trình lấy ra top 3 phần tử có giá trị lớn nhất từ dict
+
+Bài 09: Viết hàm đếm số lần xuất hiện các ký tự trong một String
+Ví dụ:
+    Input: ‘Stringings’
+    Output: {‘S’: 1, ‘t’: 1, ‘r’: 1, ’i’: 2, ‘n’: 2, ‘g’: 2, ‘s’: 1}
+
+Bài 10: Viết hàm đếm số lần xuất hiện các từ đơn trong một đoạn văn bản
+
+Bài 11: Viết chương trình để sinh ra dict mới từ list các dict có dạng như trong ví dụ:
+Ví dụ:
+    Input: [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
+    Output: {'item1': 1150, 'item2': 300}
+
+"""
