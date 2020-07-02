312389221623c1791db55bbdf3ae8b827607d208
Hoan PP <hoan.pp@teko.vn>
2020-07-02 20:59:35 +0700

    Lesson 07

---
+++ source/lesson_07/btvn.py
@@ -0,0 +1,56 @@
+""" BÀI TẬP VỀ NHÀ BUỔI 07 - Function:
+
+Bài 01: Viết hàm
+        def max_min(*numbers)
+    trả lại cả giá trị max, min của nhiều số được truyền vào
+
+Bài 02: Viết hàm
+        def reverse_string(str)
+    trả lại chuỗi đảo ngược của chuỗi str
+
+Bài 03: Viết hàm
+        def is_perfect(n)
+    để kiểm tra xem số tự nhiên n có phải là số hoàn hảo hay ko, trả lại True nếu có, False nếu không.
+    Ghi chú: Xem định nghĩa về số hoàn hảo: http://hanoimoi.com.vn/Tin-tuc/Thieu-nhi/592454/so-hoan-hao-la-gi
+
+Bài 04: Viết hàm
+        def is_prime(n)
+    để kiểm tra xem số tự nhiên n có phải số nguyên tố hay không, nếu có thì trả lại True, nếu không thì trả lại False
+
+Bài 05: Viết hàm
+        def count_upper_lower(str)
+    trả lại số lượng chữ cái viết hoa, số lượng chữ cái viết thường trong chuỗi str
+
+Bài 06: Viết hàm
+        def is_pangram(str, alphabet)
+    đề kiểm tra xem chuỗi str có phải là Pangram không, trả lại True nếu có, False nếu không
+    Ghi chú: Pangram là chuỗi chứa mỗi chữ cái trong bảng alphabet ít nhất 1 lần
+
+Bài 07: Viết hàm
+        def create_matrix(n, m)
+    xử lý việc tạo ra ma trận n hàng, m cột với giá trị phần tử tại (i, j) = i*j
+
+Bài 08: Viết hàm
+        def body_mass_index(m, h)
+    để tính toán chỉ số BMI của một người với cân nặng m (kg), chiều cao h (m)
+      Viết hàm
+        def bmi_information(m, h)
+    để đưa ra thông tin về chỉ số BMI cũng như phân loại mức độ gầy - béo của người cân nặng m, chiều cao h
+
+Bài 09: Viết hàm
+        def change_upper_lower(str)
+    chuyển toàn bộ các ký tự in hoa sang in thường và in thường thành in hoa trong str
+
+Bài 10: Viết hàm đệ quy đếm và trả về số lượng chữ số lẻ của số nguyên dương n cho trước.
+        Ví dụ: Hàm trả về 4 nếu n là 19922610 (do n có 4 số lẻ là 1, 9, 9, 1)
+
+Bài 11: Cho dãy số Tribonacci với công thức truy hồi sau:
+            F(n) = F(n-1) + F(n-2) + F(n-3),    F(1) = 1, F(2) = 1, F(3) = 2
+    Xây dựng 2 hàm để tìm ra số thứ n trong dãy số theo:
+        + Hàm Đệ quy
+        + Hàm Không đệ quy
+
+Bài 12: Viết hàm
+        def find_x(a_list, x)
+    trả lại tất cả các vị trí mà x xuất hiện trong a_list, nếu không có thì trả lại -1
+"""
