c2161ab5f5e2aa4cea7f69bd62c14c31ddfecf90
Hoan PP <hoan.pp@teko.vn>
2020-07-16 21:41:10 +0700

    lesson 09

---
+++ source/lesson_09/btvn_02_pet.py
@@ -0,0 +1,22 @@
+class Dog:
+    species = 'mammal'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def description(self):
+        return f'{self.name} is {self.age} years old'
+
+    def speak(self, sound):
+        return f'{self.name} says {sound}'
+
+
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return f'{self.name} runs {speed}'
+
+
+class Bulldog(Dog):
+    def run(self, speed):
+        return f'{self.name} runs {speed}'
