785c43dfbe15e0b6485bee3ee7576ca37f4f7098
Hoan PP <hoan.pp@teko.vn>
2020-08-20 21:25:29 +0700

    lesson05

---
+++ source/lesson_05/btvn.py
@@ -0,0 +1,30 @@
+""" BÀI TẬP VỀ NHÀ BUỔI 05 - Tuple:
+
+Bài 00: Viết chương trình sinh một tuple chứa các phần tử có các kiểu dữ liệu khác nhau.
+    Sau đó, unpack các phần tử trong một tuple.
+
+Bài 01: Viết chương trình chuyển một tuple sang thành list và ngược lại từ list sang tuple
+
+Bài 02: Viết chương trình đảo ngược một tuple.
+
+Bài 03: Viết chương trình đếm số lượng các phần tử trong một list đến khi gặp một phần tử kiểu tuple.
+
+Bài 04: Cho 1 list chứa các tuple không rỗng.
+    Viết chương trình sắp xếp list đó theo chiều tăng dần của phần tử cuối trong mỗi tuple.
+    Ví dụ: [(2, 5), (4, 1), (0, 0)]  => [(0, 0), (4, 1), (2, 5)]
+
+Bài 05: Viết chương trình tìm ra tuple có phần tử thứ 2 là nhỏ nhất từ một list chứa các tuple.
+
+Bài 06: Viết chương trình in ra phần tử thứ 4 và phần tử thứ 4 từ cuối lên trong một tuple.
+
+Bài 07: Viết chương trình kiểm tra 2 tuple có phần tử chung hay không.
+
+Bài 08: Viết chương trình kiểm tra xem tất cả các phần tử trong tuple có giống nhau hay không.
+
+Bài 09: Viết chương trình tính tổng và tìm giá trị lớn nhất trong tuple chứa các số thực.
+
+Bài 10: Cho list sau: ["www.hust.edu.vn", "www.wikipedia.org", "www.asp.net", "www.amazon.com"]
+    Viết chương trình để in ra hậu tố (vn, org, net, com) trong các tên miền website trong list trên.
+
+Bài 11: Viết chương trình tìm từ dài nhất trong một câu nhập vào từ bàn phím.
+"""
