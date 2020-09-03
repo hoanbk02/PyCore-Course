a2e025bf09d9342b2576d7b0998b3e8ade3ce79b
Hoan PP <hoan.pp@teko.vn>
2020-09-03 21:41:48 +0700

    lesson 08

---
+++ source/lesson_08/btvn.py
@@ -0,0 +1,50 @@
+""" BÀI TẬP VỀ NHÀ BUỔI 08 - File, Exception:
+
+Bài 01: Viết chương trình để đọc n dòng đầu trong 1 file text, với n được nhập từ người dùng
+
+Bài 02: Viết chương trình thêm một chuỗi nào đó vào cuối file
+
+Bài 03: Viết chương trình trộn 2 file thành file mới
+
+Bài 04: Viết hàm
+        def get_file_size(file)
+    để lấy và trả về dung lượng của file
+
+Bài 05: Viết hàm
+        def extract_characters(*file)
+    trả lại tập các ký tự trong các file
+
+Bài 06: Viết chương trình tạp ra 26 file text có tên lần lượt từ A.txt, B.txt đến Z.txt
+
+Bài 07: Đoạn chương trình sau in ra gì?
+    number = 5.0
+    try:
+        r = 10/number
+        print(r)
+    except:
+        print("Oops! Error occurred.")
+
+Bài 08: Đoạn chương trình sau thực viện việc gì?
+    try:
+        # đoạn code có thể gây ra lỗi
+    except (TypeError, ZeroDivisionError):
+        print("Python Quiz")
+
+Bài 09: Kết quả output của đoạn chương trình sau là gì?
+    def hoan_function():
+        try:
+            print('Monday')
+        finally:
+            print('Sunday')
+
+    hoan_function()
+
+Bài 10: Kết quả của đoạn chương trình sau là gì?
+    try:
+        print("throw")
+    except:
+        print("except")
+    finally:
+        print("finally")
+
+"""
