97fa6c36667e0a4df081202fd4b1533c37b10583
Hoan PP <hoan.pp@teko.vn>
2021-01-21 21:11:53 +0700

    lesson 10

---
+++ source/lesson_10/final_project.py
@@ -0,0 +1,21 @@
+"""
+Đề tài 1. Một team dự án marketing tại công ty PhongVu cần phát triển một công cụ để hỗ trợ cho việc gửi email:
+    - Gửi đơn: Gửi mail cho 1 người
+    - Gửi đa: Gửi mail cho nhiều người
+Bạn hãy tìm hiểu về cách gửi email trên Python và tiến hành xây dựng công cụ đáp ứng yêu cầu trên.
+Chú ý:
+    + Xây dựng công cụ dạng GUI là một điểm cộng
+    + Trong dạng gửi đa, danh sách email người nhận sẽ được nhập vào qua file excel
+    + Trong dạng gửi đa, hãy nghĩ đến việc mỗi người nhận, sẽ nhận được nội dung email khác nhau,
+    mang tính cá nhân của chính người nhận.
+
+Đề tài 2. Là một người dùng có nhu cầu mua lò sưởi trong những ngày Đông tới.
+Nhưng có quá nhiều trang thương mại điện tử mà mỗi đơn vị lại có một giá bán khác nhau.
+Hãy xây dựng công cụ thể thực hiện trích xuất dữ liệu từ tối thiểu 2 trang thương mại điện tử lớn để so sánh giá của các sản phẩm lò sưởi
+Chú ý:
+    + Nên xây dựng để mở rộng cho các sản phẩm khác nữa
+    + Thiết kế Cơ sở dữ liệu để lưu lại các dữ liệu đã trích xuất được
+
+Đề tài 3. Dựa trên các demo về Speech to Text và Text to Speech.
+Hãy xây dựng một chương trình trợ lý ảo đơn giản trong một chủ đề nào đó (như Thời tiết, Bật nhạc, ...)
+"""
