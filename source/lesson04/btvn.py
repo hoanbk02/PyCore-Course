ae4c978ee411797767e2718e7bb2eb3fb334bd6e
hoanpp <hoanbk02@gmail.com>
2021-06-05 23:02:47 +0700

    Lesson 04

--- source/lesson04/btvn.py
+++ source/lesson04/btvn.py
@@ -12,7 +12,7 @@ Bài 2. Lập trình thực hiện các công việc sau:
 Bài 3. Lập chương trình tính các tổng sau:
     S_1 = 1 + x + x^2 + x^3 + ... + x^n
     S_2 = 1 - x + x^2 - x^3 + ... + (-1)^n.x^n
-    S_3 = 1 + \dfrac{x}{1!} + \dfrac{x^2}{2!} + ... + \dfrac{x^n}{n!}
+    S_3 = 1 + x/1! + x^2/2! + ... + x^n/n!
     Trong đó, n là số nguyên dương và x là một số thực bất kì. Cả 2 đều được nhập từ bàn phím
 
 Bài 4. Lập chương trình thực hiện các công việc sau:
