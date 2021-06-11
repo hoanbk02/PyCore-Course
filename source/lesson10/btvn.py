3ba23994bfbd90f82ad7881f4acaaeeb7838f8ff
hoanpp <hoanbk02@gmail.com>
2021-06-11 23:32:39 +0700

    Lesson 10

--- source/lesson10/btvn.py
+++ source/lesson10/btvn.py
@@ -1,6 +1,6 @@
 """ BÀI TẬP VỀ NHÀ
 
-Bài 01: Đoạn chương trình sau in ra gì?
+Bài 01. Đoạn chương trình sau in ra gì?
     number = 5.0
     try:
         r = 10/number
@@ -8,13 +8,13 @@ Bài 01: Đoạn chương trình sau in ra gì?
     except:
         print("Oops! Error occurred.")
 
-Bài 02: Đoạn chương trình sau thực viện việc gì?
+Bài 02. Đoạn chương trình sau thực viện việc gì?
     try:
         # đoạn code có thể gây ra lỗi
     except (TypeError, ZeroDivisionError):
         print("Python Quiz")
 
-Bài 03: Kết quả output của đoạn chương trình sau là gì?
+Bài 03. Kết quả output của đoạn chương trình sau là gì?
     def hoan_function():
         try:
             print('Monday')
@@ -22,7 +22,7 @@ Bài 03: Kết quả output của đoạn chương trình sau là gì?
             print('Sunday')
     hoan_function()
 
-Bài 04: Kết quả của đoạn chương trình sau là gì?
+Bài 04. Kết quả của đoạn chương trình sau là gì?
     try:
         print("throw")
     except:
@@ -30,17 +30,17 @@ Bài 04: Kết quả của đoạn chương trình sau là gì?
     finally:
         print("finally")
 
-Bài 05: Viết chương trình để đọc n dòng đầu trong 1 file text, với n được nhập từ người dùng
+Bài 05. Viết chương trình để đọc n dòng đầu trong 1 file text, với n được nhập từ người dùng
 
-Bài 06: Viết chương trình thêm một chuỗi nào đó vào cuối file
+Bài 06. Viết chương trình thêm một chuỗi nào đó vào cuối file
 
-Bài 07: Viết chương trình trộn 2 file thành file mới
+Bài 07. Viết chương trình trộn 2 file thành file mới
 
-Bài 08: Viết hàm
+Bài 08. Viết hàm
         def get_file_size(file)
     để lấy và trả về dung lượng của file
 
-Bài 09: Viết hàm
+Bài 09. Viết hàm
         def extract_characters(*file)
     trả lại tập các ký tự trong các file
 
