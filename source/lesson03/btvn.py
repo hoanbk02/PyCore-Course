2765c9ee8c81f3ab89c5375484b4c7b47db9158b
hoanpp <hoanbk02@gmail.com>
2021-07-26 22:55:02 +0700

    Lesson 03

---
+++ source/lesson03/btvn.py
@@ -0,0 +1,15 @@
+""" BÀI TẬP VỀ NHÀ
+
+Bài 1. Code đoạn chương trình cho phép:
+    1. Nhập vào bán kình r của hình tròn
+    2. Tính diện tích hình tròn đó
+    3. In kết quả ra màn hình "Dien tich hình tron la: <giá trị>"
+
+Bài 2. Code đoạn chương trình để giải quyết các yêu cầu sau:
+    1. Nhập 3 số thực x, y, z bất kì.
+    2. Tính giá trị biểu thức F: F = (x+y+z)/(x^2+y^2+1) - |x-z*cos(y)|
+    3. Đưa giá trị tính được của F ra màn hình dưới dạng: Gia tri cua F = <gia tri>
+
+Bài 3. Xây dựng đoạn chương trình mô phỏng lại các phép toán học (càng nhiều càng tốt),
+tối thiểu cần có: cộng, trừ, nhân, chia
+"""
