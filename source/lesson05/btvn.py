1335f20f3ca3e6df920928ab874843b9b414d17f
hoanpp <hoanbk02@gmail.com>
2021-08-02 22:26:19 +0700

    Lesson 05

--- source/lesson05/btvn.py
+++ source/lesson05/btvn.py
@@ -2,15 +2,17 @@
 
 Bài 01. Viết chương trình thay thế tất cả các ký tự giống ký tự đầu tiên của một Chuỗi thành $.
 
-Bài 02. Viết chương trình để xóa bỏ ký tự thứ m trong chuỗi không rỗng, với m là số không âm, nhập từ bàn phím.
-
-Bài 03. Viết chương trình để xóa bỏ các ký tự có chỉ số là số lẻ trong một chuỗi.
-
-Bài 04. Viết chương trình sinh ra một chuỗi từ 2 ký tự đầu và 2 ký tự cuối trong chuỗi được nhập vào,
+Bài 02. Viết chương trình sinh ra một chuỗi từ 2 ký tự đầu và 2 ký tự cuối trong chuỗi được nhập vào,
     nếu độ dài chuỗi nhỏ hơn 2 thì in ra chuỗi rỗng.
 
-Bài 05. Viết chương trình tìm ra ký tự lớn nhất và ký tự nhỏ nhất của một chuỗi nhập từ bán phím.
+Bài 03. Viết chương trình tìm ra ký tự lớn nhất và ký tự nhỏ nhất của một chuỗi nhập từ bán phím.
 
-Bài 06. Viết chương trình đảo ngược từ ký tự thường sang ký tự hoa và từ ký tự hoa sang ký tự thường trong một chuỗi.
+Bài 04. Viết chương trình đảo ngược từ ký tự thường sang ký tự hoa và từ ký tự hoa sang ký tự thường trong một chuỗi.
 
+Bài 05. Viết chương trình in ra các ký tự số trong chuỗi được nhập từ bàn phím
+
+Bài 06. Viết chương trình
+    1. Nhập vào 1 chuỗi từ bàn phím
+    2. Nhập vào 1 ký tự từ bàn phím
+    3. Tìm và in ra tất cả các vị trí của ký tự vừa nhập trong chuỗi đã nhập
 """
