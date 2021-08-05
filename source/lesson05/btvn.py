6a86716271ae8f4bd72f7dcb5be7ac4014ed1a76
hoanpp <hoanbk02@gmail.com>
2021-08-05 23:34:17 +0700

    Lesson 06

--- source/lesson05/btvn.py
+++ source/lesson05/btvn.py
@@ -31,7 +31,7 @@ s_new = ""
 if len(s_04) >= 2:
     s_new = s_04[0:2] + s_04[-2:]
 print("Chuỗi mới: " + s_new)
-print('Bài 04 - DONE!')
+print('Bài 02 - DONE!')
 
 
 # Bài 03: Viết chương trình tìm ra ký tự lớn nhất và ký tự nhỏ nhất của một chuỗi nhập từ bán phím.
@@ -44,7 +44,7 @@ if s_05:
         elif c < c_min:
             c_min = c
     print(f"Ký tự lớn nhất {c_max} và nhỏ nhất {c_min}")
-print('Bài 05 - DONE!')  # Bài này có thể dùng hàm max()/min()
+print('Bài 03 - DONE!')  # Bài này có thể dùng hàm max()/min()
 
 
 # Bài 04: Viết chương trình đảo ngược từ ký tự thường sang ký tự hoa và từ ký tự hoa sang ký tự thường trong một chuỗi.
@@ -58,4 +58,24 @@ for c in s_06:
     else:
         s_new = s_new + c
 print("Chuỗi mới: " + s_new)
+print('Bài 04 - DONE!')
+
+
+# Bài 05. Viết chương trình in ra các ký tự số trong chuỗi được nhập từ bàn phím
+s = input("Nhập một chuỗi: ")
+for ky_tu in s:
+    if '0' <= ky_tu <= '9':
+        print(ky_tu, end="")
+print('Bài 05 - DONE!')
+
+
+# Bài 06. Viết chương trình
+#     1. Nhập vào 1 chuỗi từ bàn phím
+#     2. Nhập vào 1 ký tự từ bàn phím
+#     3. Tìm và in ra tất cả các vị trí của ký tự vừa nhập trong chuỗi đã nhập
+s = input("Nhập một chuỗi: ")
+a = input("Nhập vào một ký tự: ")
+for i in range(len(s)):
+    if s[i] == a:
+        print(i, end=" ")
 print('Bài 06 - DONE!')
