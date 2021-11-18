616c66e1d0a60432a6de129e307a38d7412619e9
hoanbk02 <hoanbk02@gmail.com>
2021-11-18 17:02:02 +0700

    Lesson 01, 02

--- source/lesson01/btvn.py
+++ source/lesson01/btvn.py
@@ -5,4 +5,5 @@ Bài 01. Thực hành code lại các chương trình trong bài giảng. Mỗi 
 Bài 02. Đẩy các file code của Bài 01 lên github
 
 Bài 03. Tiến hành sửa các file code của Bài 01 rồi đẩy lại lên github và kiểm tra kết quả trên github
+
 """
