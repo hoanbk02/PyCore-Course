989494311c91fe6a57b2346b8a8653455fbb2c24
hoanbk02 <hoanbk02@gmail.com>
2021-12-23 19:21:10 +0700

    Lesson 12

---
+++ source/lesson12/btvn.py
@@ -0,0 +1,48 @@
+""" BÀI TẬP VỀ NHÀ
+
+Bài 01. Đoạn chương trình sau in ra gì?
+    number = 5.0
+    try:
+        r = 10/number
+        print(r)
+    except:
+        print("Oops! Error occurred.")
+
+Bài 02. Đoạn chương trình sau thực viện việc gì?
+    try:
+        # đoạn code có thể gây ra lỗi
+    except (TypeError, ZeroDivisionError):
+        print("Python Quiz")
+
+Bài 03. Kết quả output của đoạn chương trình sau là gì?
+    def hoan_function():
+        try:
+            print('Monday')
+        finally:
+            print('Sunday')
+    hoan_function()
+
+Bài 04. Kết quả của đoạn chương trình sau là gì?
+    try:
+        print("throw")
+    except:
+        print("except")
+    finally:
+        print("finally")
+
+Bài 05. Viết chương trình để đọc n dòng đầu trong 1 file text, với n được nhập từ người dùng
+
+Bài 06. Viết chương trình thêm một chuỗi nào đó vào cuối file
+
+Bài 07. Viết chương trình trộn 2 file thành file mới
+
+Bài 08. Viết hàm
+        def get_file_size(file)
+    để lấy và trả về dung lượng của file
+
+Bài 09. Viết hàm
+        def extract_characters(*file)
+    trả lại tập các ký tự trong các file
+
+Bài 10. Viết chương trình tạo ra 26 file text có tên lần lượt từ A.txt, B.txt đến Z.txt
+"""
