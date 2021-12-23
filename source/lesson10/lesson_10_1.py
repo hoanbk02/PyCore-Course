78f65b52d9c0d9a2a99cffca08d9d0ee03c7a941
hoanbk02 <hoanbk02@gmail.com>
2021-12-23 19:20:35 +0700

    Lesson 10

--- source/lesson10/lesson_10_1.py
+++ source/lesson10/lesson_10_1.py
@@ -3,132 +3,97 @@ __author__ = "hoanbk02@gmail.com"
 __copyright__ = "Copyright 2020, Phạm Phú Hoàn"
 
 
-""" class, object là gì?
-    - Python là ngôn ngữ lập trình hướng đối tượng. Không giống như ngôn ngữ lập trình hướng thủ tục, trọng tâm là hàm,
-    lập trình hướng đối tượng nhận mạnh vào đối tượng - object
-    - object đơn giản chỉ là tập các dữ liệu (biến) và các phương thức hoạt động trên dữ liệu đó. class là bản thết kế cho object
-    - Ví dụ, class như bản phác thảo (nguyên mẫu) cho một ngồi nhà, trong đó mô tả về sàn, tường, cửa sổ, ...
-    Dựa vào đó, để đi xây dựng ngôi nhà - đây chính là object
-    Nhiều ngôi nhà có thể được tạo ra từ một bản thiết kế => tạo ra nhiều object từ một class
-    Mỗi object được gọi là instance (thể hiện) cho class, việc tạo ra object này còn được gọi là instantiation - khởi tạo
+# Ví dụ: Mô phỏng tung xúc xắc. In ra số chấm trên mặt sau khi tung một xúc xắc 10 lần
+""" Để mô phỏng được yếu tố may rủi (ngẫu nhiên) thì chúng ta có thể dùng đến module random"""
+import random
+
+for roll in range(10):
+    print(random.randrange(1, 7), end=' ')
+
+# Tìm hiểu chi tiết về hàm randrange(start, stop=None, step=1)
+
+# Ví dụ: Kiểm tra xem khả năng (hay xác suất) xảy ra của các mặt có phải thực sự ngẫu nhiên hay không
+""" Để làm được việc trên hãy tung một xúc xắc càng nhiều lần càng tốt"""
+
+import random
+
+# Các biến lưu số lần xuất hiện của các mặt
+f1 = f2 = f3 = f4 = f5 = f6 = 0
+
+n = int(input('Muốn tung bao lần: '))
+
+for roll in range(n):
+    num = random.randrange(1, 7)
+
+    if num == 1:
+        f1 += 1
+    elif num == 2:
+        f2 += 1
+    elif num == 3:
+        f3 += 1
+    elif num == 4:
+        f4 += 1
+    elif num == 5:
+        f5 += 1
+    elif num == 6:
+        f6 += 1
+
+print(f'Số chấm{"Số lần":>13}{"Xác suất":>15}')
+print(f'{1:>7}{f1:>13}{round(f1/n, 5):>15}')
+print(f'{2:>7}{f2:>13}{round(f2/n, 5):>15}')
+print(f'{3:>7}{f3:>13}{round(f3/n, 5):>15}')
+print(f'{4:>7}{f4:>13}{round(f4/n, 5):>15}')
+print(f'{5:>7}{f5:>13}{round(f5/n, 5):>15}')
+print(f'{6:>7}{f6:>13}{round(f6/n, 5):>15}')
+
+
+""" Bài tập. Mô phỏng lại trò chơi sau: 
+- Người chơi tung 2 con xúc xắc 6 mặt, các mặt có số chấm là: 1, 2, 3, 4, 5, 6
+- Quan sát kết quả, tính tổng các mặt hướng lên sau khi 2 con xúc xắc đứng yên
+- Lần đầu tiên,
+    - Nếu tổng là 7 hoặc 11 => Người chơi THẮNG
+    - Nếu tổng là 2, 3 hoặc 12 => Người chơi THUA
+    - Nếu tổng là 4, 5, 6, 8, 9 hoặc 10 => Đây sẽ là ĐIỂM của người chơi, và sang vòng tiếp theo
+- Để giành được THẮNG, người chơi tiếp tục tung 2 con xúc xắc cho đến khi ra được tổng = ĐIỂM trong lần đầu tiên; 
+nếu tung ra được tổng = 7 => Người chơi THUA
 """
 
-""" Khai báo class:
-    - Bắt đầu bằng từ khóa class, sau đó là tên của nó
-    - Đoạn string đầu tiên được gọi là docstring - mô tả về class. Dù nó là ko bắt buộc, nhưng nên viết nó
-    => như Ví dụ 1
-    - Một class sẽ tạo ra một không gian cục bộ mới, nơi mà thuộc tính - attribute của nó được định nghĩa.
-    Thuộc tính có thể là data hay function
-    - Ngoài ra, còn có các thuộc tính đặc biệt được định nghĩa bắt đầu bằng 2 gạch dưới __, ví dụ: __doc__ trả lại docstring của class đó
-    - Ngay khi định nghĩa class, thì một class object mới được tạo ra với cùng tên.
-    Cái này giúp cho phép truy cập các thuộc tính và khởi tạo đối tượng mới cho class đó => Xem ví dụ 2
-"""
-# Ví dụ 1:
-class MyClass:
-    """ This is a docstring. I have created a new class """
-    pass
+import random
+
+THANG = (7, 11)
+THUA = (2, 3, 12)
 
 
-# Ví dụ 2:
-class MyClass:
-    """ Đây là docstring của MyClass """
-    attr = 0
-
-    def func(self):
-        print('Hallo!')
-
-
-print(MyClass.__doc__)
-print(MyClass.attr)
-print(MyClass.func)
+def rolling_dice():
+    """ Tung hai xúc xắc, in ra kết quả và trả về tổng """
+    num1 = random.randrange(1, 7)
+    num2 = random.randrange(1, 7)
+    num_sum = num1 + num2
+    print(f'Tung ra: {num1} + {num2} = {num_sum}')
+    return num_sum
 
 
-""" Tạo object
-    - Mỗi class object dùng để truy cập vào thuộc tính khác nhau
-    - Nó cũng có thể dùng để tạo ra các object mới (thể hiện của class). Cách dùng như một lời gọi hàm => Ví dụ 3: Tạo ra
-    một object có tên là obj. Có thể truy cập đến các thuộc tính của object bằng cách dùng tên của object đó.
-    - Thuộc tính có thể là data hoặc method. Method của một object là các hàm tương ứng của class. Bất kì function object
-    nào là thuộc tính của class đều được xác định là một method cho object của class đó.
-    Điều này có nghĩa là MyClass.func là một function object (attribute của class), obj.func là method object
-"""
-# Ví dụ 3:
-obj = MyClass()
+# Lần chơi đầu tiên
+player_point = rolling_dice()
+if player_point in THANG:
+    game_status = 'THANG'
+elif player_point in THUA:
+    game_status = 'THUA'
+else:
+    game_status = 'TIEP_TUC'
+    print(f'Số ĐIỂM = {player_point}')
 
-# Ví dụ 4:
-class MyClass:
-    """ Đây là docstring của MyClass """
-    attr = 0
+# Tiếp tục chơi đến khi THẰNG hoặc THUA
+while game_status == 'TIEP_TUC':
+    sum_of_dice = rolling_dice()
 
-    def func(self):
-        print('Hallo!')
+    if sum_of_dice == player_point:
+        game_status = 'THANG'
+    elif sum_of_dice == 7:
+        game_status = 'THUA'
 
-
-obj = MyClass()
-print(MyClass.func)  # Truy cập vào hàm của class => chính là function object
-print(obj.func)  # Truy cập và hàm của class => chính là method object
-obj.func()  # Gọi hàm func()  # Yêu cầu object thực thi method của nó
-
-
-# Để ý, trong class, có tham số self trong định nghĩa function, nhưng khi gọi obj.func() lại ko cần tham số => nó vẫn chạy
-# Điều này là do, bất cứ khi nào một đối tượng gọi method của nó, chính là sẽ được truyền làm tham số đầu tiên
-# Vì vậy, obj.func() được dịch thành MyClass.func(obj)
-# => Túm lại thì, việc gọi phương thức với 1 list n tham số là việc gọi hàm tương ứng với list tham số được tạo ra bằng cách
-# thêm object trước tham số đầu tiên. Vì vậy, đối số đầu tiên của hàm trong class phải là chính đối tượng đó,
-# điều này được gọi là self, có thể dùng từ khác nhưng khuyên chân thành là nên tuân theo quy ước dùng self.
-# Giờ thì đã biết đến: class object, instance object, function object, method object (và sự khác nhau giữa chúng)
-
-
-""" Constructor - Hàm Khởi tạo
-    - Các hàm trong class mà được bắt đầu bằng __ được gọi là các hàm đặc biệt và chúng mang nghĩa đặc biệt
-    - Một trong số chúng cần quan tâm đặc biệt là __init__(). Nó sẽ được gọi khi một đối tượng mới được tạo ra
-    => Hàm kiểu này, trong OOP được gọi là Constructor
-    - Thường dùng để khởi tạo sẵn cho các thuộc tính
-"""
-
-
-class Fraction:
-    def __init__(self, tu=0, mau=1):
-        self.tu_so = tu
-        self.mau_so = mau
-
-    def show(self):
-        print(f'Phân số: {self.tu_so}/{self.mau_so}')
-
-
-ps1 = Fraction(1, 3)
-ps1.show()
-
-ps2 = Fraction(3, 5)
-ps2.attr = 7
-print(ps2.tu_so, ps2.mau_so, ps2.attr)
-
-print(ps1.attr)  # AttributeError: 'Fraction' object has no attribute 'attr'
-
-# Trong đoạn code trên, định nghĩa một class mới Fraction biểu diễn cho phân số, có hai hàm
-# __init__() để khởi tạo các biến tử số và mẫu số, mặc định là 0 và 1
-# và show() để hiển thị phân số đúng cách
-# Một thứ rất thú vị, thuộc tính của một đối tượng có thể được tạo ra khi đang chạy chương trình.
-# Thấy thuộc tính attr của ps2 được tạo ra và được đọc bình thường, còn ps1 thì ko có, cố tính truy cập sẽ báo lỗi
-
-
-""" Xóa bỏ thuộc tính và đối tượng
-    - Bất kì thuộc tính nào của đối tượng cũng có thể bị xóa bỏ đi bất cứ lúc nào bằng cách dùng câu lệnh del => Ví dụ 5
-"""
-
-# Ví dụ 5:
-ps3 = Fraction(2, 5)
-del ps3.mau_so
-ps3.show()  # AttributeError: 'Fraction' object has no attribute 'mau_so'
-
-del Fraction.show
-ps3.show()  # AttributeError: 'Fraction' object has no attribute 'show'
-
-ps4 = Fraction(5, 9)
-del ps4
-ps4  # NameError: name 'ps4' is not defined
-
-# Giải thích: xóa bỏ đối tượng, trong thực tế cũng khá phức tạp
-# Khi tạo ra ps4 = Fraction(5, 9) một đối tượng của Fraction được tạo ra trong memory và ps4 được gắn với nó
-# Trong lệnh, del ps4 cái liên kết gắn đó bị xóa và tên ps4 được xóa bỏ khỏi không gian các tên biến.
-# Và đối tượng thì vẫn tồn tại trong memory, khi ko còn tên nào được gắn với nó thì nó sẽ bị hủy tự động
-# Việc hủy tự động các đối tượng không có tham chiếu này trong Python được gọi là bộ sưu tập rác - garbage collection
+# In ra kết quả cuối cùng của người chơi
+if game_status == 'THANG':
+    print('Người chơi đã THẮNG')
+else:
+    print('Người chơi đã THUA')
--- source/lesson10/lesson_10_2.py
+++ source/lesson10/lesson_10_2.py
@@ -2,212 +2,133 @@
 __author__ = "hoanbk02@gmail.com"
 __copyright__ = "Copyright 2020, Phạm Phú Hoàn"
 
-""" OOPs trong Python
-    - Python là một trong những ngôn ngữ lập trình đa mô hình => Python sẽ hỗ trợ nhiều phương pháp lập trình khác nhau
-    - Một trong những phương pháp tiếp cận phổ biến hiện nay là Hướng đối tượng - Object-Oriented Programming (OOP)
-    - Một đối tượng có hai đặc trưng:
-        - Thuộc tính (attributes)
-        - Hành vi (behavior)
-    - Xét ví dụ sau:
-        Cat là một đối tượng có:
-        - tên, tuổi, màu lông => Thuộc tính
-        - kêu, chạy, nhảy, ăn, uống => Hành vi
-    - Khái niệm OOP trong Python còn tập trung vào việc viết code có thể tái sử dụng, được biết đến như là DRY - Dont Repeat Yourself
-    - Trong Python, OOP theo một số nguyên tắc cơ bản như sau:
-        - Inheritance - Kế thừa: Sử dụng các chi tiết từ một class mới mà ko làm thay đổi gì class đã có
-        - Encapsulation - Đóng gói: Ẩn các chi tiết riêng tư của một class đối với các object khác
-        - Polymorphism - Đa hình: Sử dụng các thao tác chung theo nhiều cách khác nhau tương ứng với các đầu vào khác nhau
+
+""" class, object là gì?
+    - Python là ngôn ngữ lập trình hướng đối tượng. Không giống như ngôn ngữ lập trình hướng thủ tục, trọng tâm là hàm,
+    lập trình hướng đối tượng nhận mạnh vào đối tượng - object
+    - object đơn giản chỉ là tập các dữ liệu (biến) và các phương thức hoạt động trên dữ liệu đó. class là bản thết kế cho object
+    - Ví dụ, class như bản phác thảo (nguyên mẫu) cho một ngồi nhà, trong đó mô tả về sàn, tường, cửa sổ, ...
+    Dựa vào đó, để đi xây dựng ngôi nhà - đây chính là object
+    Nhiều ngôi nhà có thể được tạo ra từ một bản thiết kế => tạo ra nhiều object từ một class
+    Mỗi object được gọi là instance (thể hiện) cho class, việc tạo ra object này còn được gọi là instantiation - khởi tạo
 """
 
-""" Class - Là bản thiết kế cho object
-    - Có thể hình dùng class như một bản phác thảo của một cat với các nhãn. Nó chứa tất cả chi tiết về name, age, ...
-    - Dựa trên các mô tả đó, chúng ta sẽ đi tìm hiểu về cat, ở đây, cat là một object
+""" Khai báo class:
+    - Bắt đầu bằng từ khóa class, sau đó là tên của nó
+    - Đoạn string đầu tiên được gọi là docstring - mô tả về class. Dù nó là ko bắt buộc, nhưng nên viết nó
+    => như Ví dụ 1
+    - Một class sẽ tạo ra một không gian cục bộ mới, nơi mà thuộc tính - attribute của nó được định nghĩa.
+    Thuộc tính có thể là data hay function
+    - Ngoài ra, còn có các thuộc tính đặc biệt được định nghĩa bắt đầu bằng 2 gạch dưới __, ví dụ: __doc__ trả lại docstring của class đó
+    - Ngay khi định nghĩa class, thì một class object mới được tạo ra với cùng tên.
+    Cái này giúp cho phép truy cập các thuộc tính và khởi tạo đối tượng mới cho class đó => Xem ví dụ 2
 """
-class Cat:
+# Ví dụ 1:
+class MyClass:
+    """ This is a docstring. I have created a new class """
     pass
-# Sử dụng keyword class để định nghĩa một class Cat - rỗng. Từ class, chúng ta sẽ khởi tạo được instance.
-# Một instance là một đối tượng riêng biệt từ lớp cụ thể
 
 
-""" Object
-    - Một object (instance) là một khởi tạo của một lớp
-    - Khi class được định nghĩa, thì chỉ có các mô tả của object được xác định
-    => không có bộ nhớ hoặc lưu trữ nào được phân bổ
+# Ví dụ 2:
+class MyClass:
+    """ Đây là docstring của MyClass """
+    attr = 0
+
+    def func(self):
+        print('Hallo!')
+
+
+print(MyClass.__doc__)
+print(MyClass.attr)
+print(MyClass.func)
+
+
+""" Tạo object
+    - Mỗi class object dùng để truy cập vào thuộc tính khác nhau
+    - Nó cũng có thể dùng để tạo ra các object mới (thể hiện của class). Cách dùng như một lời gọi hàm => Ví dụ 3: Tạo ra
+    một object có tên là obj. Có thể truy cập đến các thuộc tính của object bằng cách dùng tên của object đó.
+    - Thuộc tính có thể là data hoặc method. Method của một object là các hàm tương ứng của class. Bất kì function object
+    nào là thuộc tính của class đều được xác định là một method cho object của class đó.
+    Điều này có nghĩa là MyClass.func là một function object (attribute của class), obj.func là method object
 """
-obj_cat = Cat()
-# Ở đây, obj_cat là object của class Cat
+# Ví dụ 3:
+obj = MyClass()
 
-# Chúng ta đã nói chi tiết về Mèo - Cat rồi, giờ sẽ cũng nhau đi xây dựng chi tiết về class và về object của Cat như sau:
-class Cat:
-    # class attribute - thuộc tính của class
-    loai = 'Thú họ Mèo'
+# Ví dụ 4:
+class MyClass:
+    """ Đây là docstring của MyClass """
+    attr = 0
 
-    # instance attribute - thuộc tính của đối tượng
-    def __init__(self, name, age, color):
-        self.name = name
-        self.age = age
-        self.color = color
+    def func(self):
+        print('Hallo!')
 
 
-# instance of Cat class - Khai báo 2 đối tượng của class Cat
-muop = Cat('Mướp', 2, 'Vàng')
-mun = Cat('Mun', 1, 'Đen')
-
-# Truy cập class attribute
-print(f'Mướp là', muop.__class__.loai)
-print(f'Mun cũng là', mun.__class__.loai)
-
-# Truy cập instance attribute
-print(f'{muop.name} đã {muop.age} tuổi và lông màu {muop.color}')
-print(f'{mun.name} đã {mun.age} tuổi và lông màu {mun.color}')
-
-# Trong đoạn chương trình trên, đã tạo ra một class Cat với các attribute và các attribute là những đặc trưng của từng object
-# Sau đó, tạo 2 object của Cat là muop và mun
-# Truy cập đến thuộc tính class (class attribute) bằng __class__.loai. Các class attribute sẽ là như nhau với mọi object của Cat
-# Truy cập đến instance attribute bằng muop.name, muop.age, ... Các instance attribute của các object sẽ là khác nhau
+obj = MyClass()
+print(MyClass.func)  # Truy cập vào hàm của class => chính là function object
+print(obj.func)  # Truy cập và hàm của class => chính là method object
+obj.func()  # Gọi hàm func()  # Yêu cầu object thực thi method của nó
 
 
-""" Method - Là các định nghĩa hàm bên trong thân class, dùng để định nghĩa hành vi của object """
-
-# Ví dụ: Tạo method trong Python
-class Cat:
-    # class attribute - thuộc tính của class
-    loai = 'Thú họ Mèo'
-
-    # instance attribute - thuộc tính của đối tượng
-    def __init__(self, name, age, color):
-        self.name = name
-        self.age = age
-        self.color = color
-
-    # instance method
-    def run(self):
-        return f'{self.name} đang chạy'
-
-    def eat(self, f):
-        return f'{self.name} thích ăn {f}'
+# Để ý, trong class, có tham số self trong định nghĩa function, nhưng khi gọi obj.func() lại ko cần tham số => nó vẫn chạy
+# Điều này là do, bất cứ khi nào một đối tượng gọi method của nó, chính là sẽ được truyền làm tham số đầu tiên
+# Vì vậy, obj.func() được dịch thành MyClass.func(obj)
+# => Túm lại thì, việc gọi phương thức với 1 list n tham số là việc gọi hàm tương ứng với list tham số được tạo ra bằng cách
+# thêm object trước tham số đầu tiên. Vì vậy, đối số đầu tiên của hàm trong class phải là chính đối tượng đó,
+# điều này được gọi là self, có thể dùng từ khác nhưng khuyên chân thành là nên tuân theo quy ước dùng self.
+# Giờ thì đã biết đến: class object, instance object, function object, method object (và sự khác nhau giữa chúng)
 
 
-cam = Cat('Cam', 2, 'Vàng, Trắng')
-
-# call instance methods
-print(cam.run())
-print(cam.eat('pate gan'))
-
-# Ở trên chúng ta định nghĩa 2 method: run() và eat(), đây được gọi là instance method vì chúng chỉ được gọi qua một instance object
-
-
-""" Inheritance - Kế thừa
-    - Là cách tạo ra class mới có sử dụng chi tiết từ class đã có mà ko thay đổi nó
-    - Lớp mới được gọi là lớp dẫn xuất (hoặc lớp con), lớp đã có là lớp cơ sở (hoặc lớp cha)
+""" Constructor - Hàm Khởi tạo
+    - Các hàm trong class mà được bắt đầu bằng __ được gọi là các hàm đặc biệt và chúng mang nghĩa đặc biệt
+    - Một trong số chúng cần quan tâm đặc biệt là __init__(). Nó sẽ được gọi khi một đối tượng mới được tạo ra
+    => Hàm kiểu này, trong OOP được gọi là Constructor
+    - Thường dùng để khởi tạo sẵn cho các thuộc tính
 """
-# Ví dụ:
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        print('Tạo Person!')
-
-    def who_am_i(self):
-        print('Person')
-
-    def speak(self, lang='Tiếng Việt'):
-        print(f'{self.name} nói {lang}')
 
 
-class StudentIT(Person):
-    def __init__(self, name, age, school):
-        super().__init__(name, age)
-        self.school = school
-        print('Tạo StudentIT!')
+class Fraction:
+    def __init__(self, tu=0, mau=1):
+        self.tu_so = tu
+        self.mau_so = mau
 
-    def who_am_i(self):
-        print('StudentIT')
-
-    def code(self, lang):
-        print(f'{self.name} lập trình {lang}')
+    def show(self):
+        print(f'Phân số: {self.tu_so}/{self.mau_so}')
 
 
-svbk = StudentIT('CườngĐM', 30, 'HUST')
-svbk.who_am_i()
-svbk.speak()
-svbk.code('Python')
+ps1 = Fraction(1, 3)
+ps1.show()
 
-# Trong đoạn chương trình trên, chúng ta tạo ra 2 class: Person (cha) và StudentIT (con).
-# Class con kế thừa toàn bộ các hàm của class cha, có thể nhìn thấy method speak()
-# Tiếp đến, class con đã thay đổi hành vi của class cha với method who_am_i()
-# Hơn nữa, class con mở rộng class cha với method code()
-# Thêm đó, sử dụng super().__init__() vì muốn kéo toàn bộ __init__() của class cha vào class con
+ps2 = Fraction(3, 5)
+ps2.attr = 7
+print(ps2.tu_so, ps2.mau_so, ps2.attr)
+
+print(ps1.attr)  # AttributeError: 'Fraction' object has no attribute 'attr'
+
+# Trong đoạn code trên, định nghĩa một class mới Fraction biểu diễn cho phân số, có hai hàm
+# __init__() để khởi tạo các biến tử số và mẫu số, mặc định là 0 và 1
+# và show() để hiển thị phân số đúng cách
+# Một thứ rất thú vị, thuộc tính của một đối tượng có thể được tạo ra khi đang chạy chương trình.
+# Thấy thuộc tính attr của ps2 được tạo ra và được đọc bình thường, còn ps1 thì ko có, cố tính truy cập sẽ báo lỗi
 
 
-""" Encapsulation - Đóng gói
-    - Trong Python, có thể hạn chế quyền truy cập vào các phương thức và biến => ngăn dữ liệu ko bị sửa đổi trực tiếp
-    => được gọi là Đóng gói - Encapsulation
-    - Dùng ký hiệu cho thuộc tính private là 2 gạch dưới __
+""" Xóa bỏ thuộc tính và đối tượng
+    - Bất kì thuộc tính nào của đối tượng cũng có thể bị xóa bỏ đi bất cứ lúc nào bằng cách dùng câu lệnh del => Ví dụ 5
 """
-# Ví dụ
-class Laptop:
 
-    def __init__(self):
-        self.__price = 500
+# Ví dụ 5:
+ps3 = Fraction(2, 5)
+del ps3.mau_so
+ps3.show()  # AttributeError: 'Fraction' object has no attribute 'mau_so'
 
-    def sell(self):
-        print(f'Giá bán là {self.__price}')
+del Fraction.show
+ps3.show()  # AttributeError: 'Fraction' object has no attribute 'show'
 
-    def set_price(self, price):  # setter
-        self.__price = price
+ps4 = Fraction(5, 9)
+del ps4
+ps4  # NameError: name 'ps4' is not defined
 
-
-lap = Laptop()
-lap.sell()
-
-# thay đổi giá trị price, cố tình truy cập vào thuộc tính
-lap.__price = 1000
-lap.sell()
-
-# thay đổi qua hàm setter
-lap.set_price(1000)
-lap.sell()
-
-# Trong chương trình trên, khai báo một class Laptop, trong method __init__() lưu giá của laptop đó.
-# Cố gắng thay đổi giá, tuy nhiên, ko thể, vì python coi __price là thuộc tính private
-# Để thay đổi giá thì ta dùng qua setter: set_price() với tham số là giá mới
-# Cách để lấy giá của các thuộc tính ra thì dùng hàm getter, ở đây chính là sell()
-
-
-""" Polymorphism - Đa hình
-    - Là khả năng (trong OOP) cho phép dùng chung interface cho nhiều hình thái (kiểu dữ liệu)
-    - Ví dụ cho dễ hiểu: Cần tô màu cho một hình, có nhiều hình: vuông, tròn, chữ nhật, ...; nhưng có thể dùng cùng một
-    phương thức để tô màu cho hình => Đây chính là đa hình
-"""
-# Ví dụ:
-class PC:
-    def wifi(self):
-        print('Không kết nối được!')
-
-
-class Laptop:
-    def wifi(self):
-        print('Kết nối được rồi!')
-
-
-def connect_wifi(computer):
-    computer.wifi()
-
-
-lap = Laptop()
-pc = PC()
-
-connect_wifi(lap)
-connect_wifi(pc)
-
-# Trong đoạn chương trình trên, tạo 2 class PC và Laptop, mỗi cái đều có method wifi(); tuy nhiên, chúng khác nhau
-# Để kiểm tra về đa hình, tạo ra hàm connect_wifi() nhận bất kì object.
-# Truyền vào hàm đó đối tượng lap và pc và chạy nó xem kết quả.
-
-""" Lưu ý:
-    - Lập trình trở lên dễ dàng và hiệu quả
-    - Class có thể chia sẻ, nên code có thể sử dụng lại
-    - Năng suất của chương trình tăng
-    - Dữ liệu được an toàn và bảo mật với sự trừu tượng hóa dữ liệu
-"""
+# Giải thích: xóa bỏ đối tượng, trong thực tế cũng khá phức tạp
+# Khi tạo ra ps4 = Fraction(5, 9) một đối tượng của Fraction được tạo ra trong memory và ps4 được gắn với nó
+# Trong lệnh, del ps4 cái liên kết gắn đó bị xóa và tên ps4 được xóa bỏ khỏi không gian các tên biến.
+# Và đối tượng thì vẫn tồn tại trong memory, khi ko còn tên nào được gắn với nó thì nó sẽ bị hủy tự động
+# Việc hủy tự động các đối tượng không có tham chiếu này trong Python được gọi là bộ sưu tập rác - garbage collection
