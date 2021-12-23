6a6ba10de623ac930c10214e47eae1e5081e31fa
hoanbk02 <hoanbk02@gmail.com>
2021-12-23 19:20:56 +0700

    Lesson 11

--- source/lesson11/lesson_11_1.py
+++ source/lesson11/lesson_11_1.py
@@ -2,181 +2,212 @@
 __author__ = "hoanbk02@gmail.com"
 __copyright__ = "Copyright 2020, Phạm Phú Hoàn"
 
-
-""" Inheritance - Kế thừa là gì?
-    - Kế thừa là một tính năng rất mạnh mẽ trong lập trình hướng đối tượng OOP
-    - Đề cập đến việc tạo ra một class mới bằng việc ít hoặc không sửa đổi một class đã có.
-    class mới được gọi là class dẫn xuất (con), class mà nó kế thừa được gọi là class cơ sở (cha)
-    - Cú pháp:
-        class BaseClass:
-            thân_của_BaseClass
-
-        class DerivedClass(BaseClass):
-            thân_của_DerivedClass
-    - class dẫn xuất thừa hưởng toàn bộ từ class cơ sở (=> Khả năng tái sử dụng code), và có thể được thêm các tính năng mới
+""" OOPs trong Python
+    - Python là một trong những ngôn ngữ lập trình đa mô hình => Python sẽ hỗ trợ nhiều phương pháp lập trình khác nhau
+    - Một trong những phương pháp tiếp cận phổ biến hiện nay là Hướng đối tượng - Object-Oriented Programming (OOP)
+    - Một đối tượng có hai đặc trưng:
+        - Thuộc tính (attributes)
+        - Hành vi (behavior)
+    - Xét ví dụ sau:
+        Cat là một đối tượng có:
+        - tên, tuổi, màu lông => Thuộc tính
+        - kêu, chạy, nhảy, ăn, uống => Hành vi
+    - Khái niệm OOP trong Python còn tập trung vào việc viết code có thể tái sử dụng, được biết đến như là DRY - Dont Repeat Yourself
+    - Trong Python, OOP theo một số nguyên tắc cơ bản như sau:
+        - Inheritance - Kế thừa: Sử dụng các chi tiết từ một class mới mà ko làm thay đổi gì class đã có
+        - Encapsulation - Đóng gói: Ẩn các chi tiết riêng tư của một class đối với các object khác
+        - Polymorphism - Đa hình: Sử dụng các thao tác chung theo nhiều cách khác nhau tương ứng với các đầu vào khác nhau
 """
-# Ví dụ về kế thừa
 
-class DaGiac:
-    """ class mô tả về đa giác là hình kín, có từ 3 cạnh trở lên,
-        thuộc tính n - lưu số cạnh,
-        thuộc tính sides - lưu độ dài các cạnh
-        phương thức input_sides - yêu cầu người dùng nhập độ dài các cạnh
-        phương thức display - hiển thị độ dài các cạnh
-        phương thức perimeter - tính và hiển thị chu vi
-    """
-    def __init__(self, no_sides):
-        self.n = no_sides
-        self.sides = [0 for i in range(no_sides)]
-
-    def input_sides(self):
-        self.sides = [float(input('Nhập cạnh ' + str(i+1) + ': ')) for i in range(self.n)]
-
-    def display(self):
-        for i in range(self.n):
-            print(f'Side {str(i+1)} is {self.sides[i]}')
-
-    def perimeter(self):
-        print(f'Chu vi là {sum(self.sides)}')
+""" Class - Là bản thiết kế cho object
+    - Có thể hình dùng class như một bản phác thảo của một cat với các nhãn. Nó chứa tất cả chi tiết về name, age, ...
+    - Dựa trên các mô tả đó, chúng ta sẽ đi tìm hiểu về cat, ở đây, cat là một object
+"""
+class Cat:
+    pass
+# Sử dụng keyword class để định nghĩa một class Cat - rỗng. Từ class, chúng ta sẽ khởi tạo được instance.
+# Một instance là một đối tượng riêng biệt từ lớp cụ thể
 
 
-import math
+""" Object
+    - Một object (instance) là một khởi tạo của một lớp
+    - Khi class được định nghĩa, thì chỉ có các mô tả của object được xác định
+    => không có bộ nhớ hoặc lưu trữ nào được phân bổ
+"""
+obj_cat = Cat()
+# Ở đây, obj_cat là object của class Cat
+
+# Chúng ta đã nói chi tiết về Mèo - Cat rồi, giờ sẽ cũng nhau đi xây dựng chi tiết về class và về object của Cat như sau:
+class Cat:
+    # class attribute - thuộc tính của class
+    loai = 'Thú họ Mèo'
+
+    # instance attribute - thuộc tính của đối tượng
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
 
 
-class TamGiac(DaGiac):
-    """ class mô tả về tam giác, kế thừa từ đa giác
-        có thêm phương thức area để tính diện tích của nó
-    """
+# instance of Cat class - Khai báo 2 đối tượng của class Cat
+muop = Cat('Mướp', 2, 'Vàng')
+mun = Cat('Mun', 1, 'Đen')
+
+# Truy cập class attribute
+print(f'Mướp là', muop.__class__.loai)
+print(f'Mun cũng là', mun.__class__.loai)
+
+# Truy cập instance attribute
+print(f'{muop.name} đã {muop.age} tuổi và lông màu {muop.color}')
+print(f'{mun.name} đã {mun.age} tuổi và lông màu {mun.color}')
+
+# Trong đoạn chương trình trên, đã tạo ra một class Cat với các attribute và các attribute là những đặc trưng của từng object
+# Sau đó, tạo 2 object của Cat là muop và mun
+# Truy cập đến thuộc tính class (class attribute) bằng __class__.loai. Các class attribute sẽ là như nhau với mọi object của Cat
+# Truy cập đến instance attribute bằng muop.name, muop.age, ... Các instance attribute của các object sẽ là khác nhau
+
+
+""" Method - Là các định nghĩa hàm bên trong thân class, dùng để định nghĩa hành vi của object """
+
+# Ví dụ: Tạo method trong Python
+class Cat:
+    # class attribute - thuộc tính của class
+    loai = 'Thú họ Mèo'
+
+    # instance attribute - thuộc tính của đối tượng
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.color = color
+
+    # instance method
+    def run(self):
+        return f'{self.name} đang chạy'
+
+    def eat(self, f):
+        return f'{self.name} thích ăn {f}'
+
+
+cam = Cat('Cam', 2, 'Vàng, Trắng')
+
+# call instance methods
+print(cam.run())
+print(cam.eat('pate gan'))
+
+# Ở trên chúng ta định nghĩa 2 method: run() và eat(), đây được gọi là instance method vì chúng chỉ được gọi qua một instance object
+
+
+""" Inheritance - Kế thừa
+    - Là cách tạo ra class mới có sử dụng chi tiết từ class đã có mà ko thay đổi nó
+    - Lớp mới được gọi là lớp dẫn xuất (hoặc lớp con), lớp đã có là lớp cơ sở (hoặc lớp cha)
+"""
+# Ví dụ:
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print('Tạo Person!')
+
+    def who_am_i(self):
+        print('Person')
+
+    def speak(self, lang='Tiếng Việt'):
+        print(f'{self.name} nói {lang}')
+
+
+class StudentIT(Person):
+    def __init__(self, name, age, school):
+        super().__init__(name, age)
+        self.school = school
+        print('Tạo StudentIT!')
+
+    def who_am_i(self):
+        print('StudentIT')
+
+    def code(self, lang):
+        print(f'{self.name} lập trình {lang}')
+
+
+svbk = StudentIT('CườngĐM', 30, 'HUST')
+svbk.who_am_i()
+svbk.speak()
+svbk.code('Python')
+
+# Trong đoạn chương trình trên, chúng ta tạo ra 2 class: Person (cha) và StudentIT (con).
+# Class con kế thừa toàn bộ các hàm của class cha, có thể nhìn thấy method speak()
+# Tiếp đến, class con đã thay đổi hành vi của class cha với method who_am_i()
+# Hơn nữa, class con mở rộng class cha với method code()
+# Thêm đó, sử dụng super().__init__() vì muốn kéo toàn bộ __init__() của class cha vào class con
+
+
+""" Encapsulation - Đóng gói
+    - Trong Python, có thể hạn chế quyền truy cập vào các phương thức và biến => ngăn dữ liệu ko bị sửa đổi trực tiếp
+    => được gọi là Đóng gói - Encapsulation
+    - Dùng ký hiệu cho thuộc tính private là 2 gạch dưới __
+"""
+# Ví dụ
+class Laptop:
 
     def __init__(self):
-        DaGiac.__init__(self, 3)
+        self.__price = 500
 
-    def area(self):
-        a, b, c = self.sides
-        p = (a + b + c)/2
-        area = math.sqrt(p * (p - a) * (p - b) * (p - c))
-        print('Diện tích của Tam giác là: %0.5f' % area)
+    def sell(self):
+        print(f'Giá bán là {self.__price}')
 
-
-tamgiac = TamGiac()
-tamgiac.input_sides()
-tamgiac.display()
-tamgiac.perimeter()
-tamgiac.area()
-
-# Không định nghĩa phương thức input_sides(), display(), perimeter() cho class TamGiac nhưng vẫn có thể dùng bình thường
-# Khi 1 thuộc tính không có trong class, sẽ tìm kiếm lên class cơ sở, và cứ thế tiếp tục nếu như class cơ sở là dẫn xuất của class khác
+    def set_price(self, price):  # setter
+        self.__price = price
 
 
-""" Method Overriding - Ghi đè phương thức
-    - Trong chương trình trên thấy, __init__() được định nghĩa trong cả 2 class.
-    - Điều gì xảy ra, khi method trong class dẫn xuất ghi đề lên class cơ sở
-    => __init__() trong lớp dẫn xuất được ưu tiên hơn trong class cơ sở
-    - Khi ghi đè, thường chúng ta mong muốn mở rộng nó chứ không đơn thuần là thay thế nó.
-    Điều tương tự cũng được thực hiện khi gọi phương thức class cơ sở từ trong class dẫn xuất DaGiac.__init__(self, 3) từ __init__() của TamGiac
-    Một cách tốt hơn làm việc này là dùng super()
-            DaGiac.__init__(self, 3)  <=>  super().__init__(3)
+lap = Laptop()
+lap.sell()
+
+# thay đổi giá trị price, cố tình truy cập vào thuộc tính
+lap.__price = 1000
+lap.sell()
+
+# thay đổi qua hàm setter
+lap.set_price(1000)
+lap.sell()
+
+# Trong chương trình trên, khai báo một class Laptop, trong method __init__() lưu giá của laptop đó.
+# Cố gắng thay đổi giá, tuy nhiên, ko thể, vì python coi __price là thuộc tính private
+# Để thay đổi giá thì ta dùng qua setter: set_price() với tham số là giá mới
+# Cách để lấy giá của các thuộc tính ra thì dùng hàm getter, ở đây chính là sell()
+
+
+""" Polymorphism - Đa hình
+    - Là khả năng (trong OOP) cho phép dùng chung interface cho nhiều hình thái (kiểu dữ liệu)
+    - Ví dụ cho dễ hiểu: Cần tô màu cho một hình, có nhiều hình: vuông, tròn, chữ nhật, ...; nhưng có thể dùng cùng một
+    phương thức để tô màu cho hình => Đây chính là đa hình
 """
-""" Python cung cấp isinstance() và issubclass() để kiểm tra tính kế thừa
-    isinstance() - trả lại True nếu object là thể hiện của class hoặc của class dẫn xuất từ nó.
-                   Tất cả các class trong Python đều được kế thừa từ class có tên là object
-    issubclass() - kiểm tra kế thừa của 2 class
+# Ví dụ:
+class PC:
+    def wifi(self):
+        print('Không kết nối được!')
+
+
+class Laptop:
+    def wifi(self):
+        print('Kết nối được rồi!')
+
+
+def connect_wifi(computer):
+    computer.wifi()
+
+
+lap = Laptop()
+pc = PC()
+
+connect_wifi(lap)
+connect_wifi(pc)
+
+# Trong đoạn chương trình trên, tạo 2 class PC và Laptop, mỗi cái đều có method wifi(); tuy nhiên, chúng khác nhau
+# Để kiểm tra về đa hình, tạo ra hàm connect_wifi() nhận bất kì object.
+# Truyền vào hàm đó đối tượng lap và pc và chạy nó xem kết quả.
+
+""" Lưu ý:
+    - Lập trình trở lên dễ dàng và hiệu quả
+    - Class có thể chia sẻ, nên code có thể sử dụng lại
+    - Năng suất của chương trình tăng
+    - Dữ liệu được an toàn và bảo mật với sự trừu tượng hóa dữ liệu
 """
-
-print(isinstance(tamgiac, TamGiac))
-print(isinstance(tamgiac, DaGiac))
-print(isinstance(tamgiac, int))
-print(isinstance(tamgiac, object))
-
-print(issubclass(TamGiac, DaGiac))
-print(issubclass(DaGiac, TamGiac))
-print(issubclass(bool, int))
-
-
-""" Đa kế thừa - Multiple Inheritance
-    - Giống như một số ngôn ngữ lập trình khác (như C++, ...), Python cung cấp đa kế thừa
-    - Một class có thể được kế thừa từ nhiều class cơ sở
-    - Tất cả các thứ của các lớp cơ sở đều kế thừa lại cho class dẫn xuất
-    - Cú pháp:
-        class Base1:
-            thân_của_Base1
-
-        class Base2:
-            thân_của_Base2
-
-        class MultiDerived(Base1, Base2):
-            thân_của_MultiDerived
-"""
-
-""" Kế thừa đa cấp - Multilevel Inheritance
-    - Có thể dùng kế thừa từ một lớp dẫn xuất => kế thừa đa cấp. Trong Python, độ sâu bao nhiêu cũng được
-    - Trong kế thừa đa cấp, toàn bộ trong class cơ sở và class dẫn xuất được kế thừa lại cho class dẫn xuất mới
-    - Cú pháp:
-        class Base:
-            thân_của_Base
-
-        class Derived1(Base):
-            thân_của_Derived1
-
-        class Derived2(Derived1):
-            thân_của_Derived2
-"""
-
-""" Thứ tự các Resolution
-    - Mọi class trong Python đều kế thừa từ class object - đây là class cơ sở nhất
-    - Về mặt kỹ thuật, tất cả các class (kể cả dựng sẵn hay do người dùng định nghĩa) đều là class dẫn xuất của class object,
-    và tất cả các đối tượng đều là thể hiện của class object => Ví dụ dưới
-    - Trong kích bản đa kế thừa, bất kì thuộc tính nào đầu tiên cũng được tìm kiếm tại class hiện tại, nếu không thấy,
-    sẽ tìm kiếm lên các lớp cha theo độ ưu tiên: chiều sâu - trước rồi từ trái qua phải, đảm bảo ko tìm kiếm cùng 1 class 2 lần
-    Ví dụ: MultiDerived được tìm kiếm theo thứ tự [Base1, Base2, object] - tuyến tính hóa của class MultiDerived,
-    quy tắc ngày được gọi là Method Resolution Order (MRO) - Thứ tự phân giải phương thức
-    - MRO đảm bảo 1 class luôn xuất hiện trước cha của nó. Được hiển thị bằng thuộc tính __mro__ hoặc phương thức mro() => Ví dụ dưới
-"""
-
-print(issubclass(list, object))
-print(isinstance(1992, object))
-print(isinstance("Python", object))
-
-
-class Base1:
-    pass
-
-
-class Base2:
-    pass
-
-
-class MultiDerived(Base1, Base2):
-    pass
-
-
-print(MultiDerived.__mro__)
-print(MultiDerived.mro())
-
-""" Một ví dụ phức tạp hơn 1 chút cho MRO - hình diễn giải: mro.jpg """
-
-class X:
-    pass
-
-
-class Y:
-    pass
-
-
-class Z:
-    pass
-
-
-class A(X, Y):
-    pass
-
-
-class B(Y, Z):
-    pass
-
-
-class M(B, A, Z):
-    pass
-
-
-print(M.mro())
--- source/lesson11/lesson_11_2.py
+++ source/lesson11/lesson_11_2.py
@@ -3,131 +3,180 @@ __author__ = "hoanbk02@gmail.com"
 __copyright__ = "Copyright 2020, Phạm Phú Hoàn"
 
 
-""" Class or Statíc Variable:
-    - Được khai báo trong class nhưng bên ngoài các method
-    - Được truy cập thông qua class chứ không cần object
-    - Biến class/static là khác biệt và không conflict với các thuộc tính khác cùng tên
+""" Inheritance - Kế thừa là gì?
+    - Kế thừa là một tính năng rất mạnh mẽ trong lập trình hướng đối tượng OOP
+    - Đề cập đến việc tạo ra một class mới bằng việc ít hoặc không sửa đổi một class đã có.
+    class mới được gọi là class dẫn xuất (con), class mà nó kế thừa được gọi là class cơ sở (cha)
+    - Cú pháp:
+        class BaseClass:
+            thân_của_BaseClass
+
+        class DerivedClass(BaseClass):
+            thân_của_DerivedClass
+    - class dẫn xuất thừa hưởng toàn bộ từ class cơ sở (=> Khả năng tái sử dụng code), và có thể được thêm các tính năng mới
+"""
+# Ví dụ về kế thừa
+
+class DaGiac:
+    """ class mô tả về đa giác là hình kín, có từ 3 cạnh trở lên,
+        thuộc tính n - lưu số cạnh,
+        thuộc tính sides - lưu độ dài các cạnh
+        phương thức input_sides - yêu cầu người dùng nhập độ dài các cạnh
+        phương thức display - hiển thị độ dài các cạnh
+        phương thức perimeter - tính và hiển thị chu vi
+    """
+    def __init__(self, no_sides):
+        self.n = no_sides
+        self.sides = [0 for i in range(no_sides)]
+
+    def input_sides(self):
+        self.sides = [float(input('Nhập cạnh ' + str(i+1) + ': ')) for i in range(self.n)]
+
+    def display(self):
+        for i in range(self.n):
+            print(f'Side {str(i+1)} is {self.sides[i]}')
+
+    def perimeter(self):
+        print(f'Chu vi là {sum(self.sides)}')
+
+
+import math
+
+
+class TamGiac(DaGiac):
+    """ class mô tả về tam giác, kế thừa từ đa giác
+        có thêm phương thức area để tính diện tích của nó
+    """
+
+    def __init__(self):
+        DaGiac.__init__(self, 3)
+
+    def area(self):
+        a, b, c = self.sides
+        p = (a + b + c)/2
+        area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+        print('Diện tích của Tam giác là: %0.5f' % area)
+
+
+tamgiac = TamGiac()
+tamgiac.input_sides()
+tamgiac.display()
+tamgiac.perimeter()
+tamgiac.area()
+
+# Không định nghĩa phương thức input_sides(), display(), perimeter() cho class TamGiac nhưng vẫn có thể dùng bình thường
+# Khi 1 thuộc tính không có trong class, sẽ tìm kiếm lên class cơ sở, và cứ thế tiếp tục nếu như class cơ sở là dẫn xuất của class khác
+
+
+""" Method Overriding - Ghi đè phương thức
+    - Trong chương trình trên thấy, __init__() được định nghĩa trong cả 2 class.
+    - Điều gì xảy ra, khi method trong class dẫn xuất ghi đề lên class cơ sở
+    => __init__() trong lớp dẫn xuất được ưu tiên hơn trong class cơ sở
+    - Khi ghi đè, thường chúng ta mong muốn mở rộng nó chứ không đơn thuần là thay thế nó.
+    Điều tương tự cũng được thực hiện khi gọi phương thức class cơ sở từ trong class dẫn xuất DaGiac.__init__(self, 3) từ __init__() của TamGiac
+    Một cách tốt hơn làm việc này là dùng super()
+            DaGiac.__init__(self, 3)  <=>  super().__init__(3)
+"""
+""" Python cung cấp isinstance() và issubclass() để kiểm tra tính kế thừa
+    isinstance() - trả lại True nếu object là thể hiện của class hoặc của class dẫn xuất từ nó.
+                   Tất cả các class trong Python đều được kế thừa từ class có tên là object
+    issubclass() - kiểm tra kế thừa của 2 class
 """
 
+print(isinstance(tamgiac, TamGiac))
+print(isinstance(tamgiac, DaGiac))
+print(isinstance(tamgiac, int))
+print(isinstance(tamgiac, object))
 
-class Pet:
-    # class variable
-    quantity = 0
-
-    def __init__(self, name, quantity):
-        self.name = name
-        self.quantity = quantity
-        Pet.quantity += quantity
-
-
-dog = Pet('Dog', 5)
-cat = Pet('Cat', 4)
-python = Pet('Python', 2)
-
-print(f"Dog: {dog.quantity}")
-print(f"Cat: {cat.quantity}")
-print(f"Python: {python.quantity}")
-
-print(f"Total: {Pet.quantity}")
-# Cách khác
-print(f"{dog.__class__.quantity}")
-print(f"{cat.__class__.quantity}")
-print(f"{python.__class__.quantity}")
+print(issubclass(TamGiac, DaGiac))
+print(issubclass(DaGiac, TamGiac))
+print(issubclass(bool, int))
 
 
-""" Minh hoạ cho việc thay đổi giá trị của class variable """
+""" Đa kế thừa - Multiple Inheritance
+    - Giống như một số ngôn ngữ lập trình khác (như C++, ...), Python cung cấp đa kế thừa
+    - Một class có thể được kế thừa từ nhiều class cơ sở
+    - Tất cả các thứ của các lớp cơ sở đều kế thừa lại cho class dẫn xuất
+    - Cú pháp:
+        class Base1:
+            thân_của_Base1
 
+        class Base2:
+            thân_của_Base2
 
-class Demo:
-    class_var = 10
-
-
-print(Demo.class_var)  # 10
-
-# Truy cập qua object
-obj_demo = Demo()
-print(obj_demo.class_var)  # 10
-
-# Thay đổi qua object
-obj_demo.class_var = 100
-print(obj_demo.class_var)  # 100
-print(Demo.class_var)  # 10
-
-# Thay đổi qua class
-Demo.class_var = 50
-print(obj_demo.class_var)  # 100
-print(Demo.class_var)  # 50
-
-# Đối với object, class_var là của nó và khi thay đổi qua object thì chỉ thay đổi class_var của object đó thôi
-# Tương tự, khi mà thay đổi class_var qua class thì chỉ thay đổi class_var của class thôi
-# => Do vậy, có thể hình dung, chúng ta có 2 biến khác nhau trong cùng class có cùng tên: một static và một ordinary (thông thường)
-
-
-""" Static Method trong Python: có 2 cách để định nghĩa static methods trong class """
-
-""" @staticmethod:
-    - Với việc dùng decorated trên thì tạo ra static method, và chia sẻ namespace với class
-    - Method này không yêu cầu tham số bắt buộc nào, còn đối với object method thì có self là bắt buộc
-    - Truy cập được vào các biến tĩnh của lớp
+        class MultiDerived(Base1, Base2):
+            thân_của_MultiDerived
 """
 
+""" Kế thừa đa cấp - Multilevel Inheritance
+    - Có thể dùng kế thừa từ một lớp dẫn xuất => kế thừa đa cấp. Trong Python, độ sâu bao nhiêu cũng được
+    - Trong kế thừa đa cấp, toàn bộ trong class cơ sở và class dẫn xuất được kế thừa lại cho class dẫn xuất mới
+    - Cú pháp:
+        class Base:
+            thân_của_Base
 
-class ExampleStatic:
-    name = 'ExampleStatic'
+        class Derived1(Base):
+            thân_của_Derived1
 
-    @staticmethod
-    def static_method():
-        print(f'{ExampleStatic.name} static_method() called')
-
-
-class ChildExampleStatic1(ExampleStatic):
-    name = 'ChildExampleStatic1'
-
-
-class ChildExampleStatic2(ExampleStatic):
-    name = 'ChildExampleStatic2'
-
-    @staticmethod
-    def static_method():
-        print(f'{ChildExampleStatic2.name} static_method() called')
-
-
-ExampleStatic.static_method()  # ExampleStatic
-ChildExampleStatic1.static_method()  # ExampleStatic
-ChildExampleStatic2.static_method()  # ChildExampleStatic2
-
-
-""" @classmethod:
-    - Khác với @staticmethod, thì @classmethod nhận một tham số bắt buộc - cls mà nó được gọi
+        class Derived2(Derived1):
+            thân_của_Derived2
 """
 
-
-class ExampleStatic:
-    name = 'ExampleStatic'
-
-    @classmethod
-    def static_method(cls):
-        print(f'{cls.name} static_method() called')
-
-
-class ChildExampleStatic1(ExampleStatic):
-    name = 'ChildExampleStatic1'
-
-
-class ChildExampleStatic2(ExampleStatic):
-    name = 'ChildExampleStatic2'
-
-    @classmethod
-    def static_method(cls):
-        print(f'{cls.name} static_method() called')
-
-
-ExampleStatic.static_method()  # ExampleStatic
-ChildExampleStatic1.static_method()  # ChildExampleStatic1
-ChildExampleStatic2.static_method()  # ChildExampleStatic2
-
-
-""" Which one should you use?
-    - Lựa chọn đầu, cho phép truy cập vào biến tĩnh trong cùng một lớp
-    - Hướng tiếp cận 2, cho phép sửa đổi class variable của các lớp con mà không cần viết lại method khi dùng Kế Thừa
+""" Thứ tự các Resolution
+    - Mọi class trong Python đều kế thừa từ class object - đây là class cơ sở nhất
+    - Về mặt kỹ thuật, tất cả các class (kể cả dựng sẵn hay do người dùng định nghĩa) đều là class dẫn xuất của class object,
+    và tất cả các đối tượng đều là thể hiện của class object => Ví dụ dưới
+    - Trong kích bản đa kế thừa, bất kì thuộc tính nào đầu tiên cũng được tìm kiếm tại class hiện tại, nếu không thấy,
+    sẽ tìm kiếm lên các lớp cha theo độ ưu tiên: chiều sâu - trước rồi từ trái qua phải, đảm bảo ko tìm kiếm cùng 1 class 2 lần
+    Ví dụ: MultiDerived được tìm kiếm theo thứ tự [Base1, Base2, object] - tuyến tính hóa của class MultiDerived,
+    quy tắc ngày được gọi là Method Resolution Order (MRO) - Thứ tự phân giải phương thức
+    - MRO đảm bảo 1 class luôn xuất hiện trước cha của nó. Được hiển thị bằng thuộc tính __mro__ hoặc phương thức mro() => Ví dụ dưới
 """
+
+print(issubclass(list, object))
+print(isinstance(1992, object))
+print(isinstance("Python", object))
+
+
+class Base1:
+    pass
+
+
+class Base2:
+    pass
+
+
+class MultiDerived(Base1, Base2):
+    pass
+
+
+print(MultiDerived.__mro__)
+print(MultiDerived.mro())
+
+""" Một ví dụ phức tạp hơn 1 chút cho MRO - hình diễn giải: mro.jpg """
+
+class X:
+    pass
+
+
+class Y:
+    pass
+
+
+class Z:
+    pass
+
+
+class A(X, Y):
+    pass
+
+
+class B(Y, Z):
+    pass
+
+
+class M(B, A, Z):
+    pass
+
+
+print(M.mro())
