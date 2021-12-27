00e270f813d835d110d6875997e505512e5b2224
hoanbk02 <hoanbk02@gmail.com>
2021-12-27 20:30:04 +0700

    Lesson Demo

---
+++ source/lesson_demo/speech2text.py
@@ -0,0 +1,28 @@
+"""
+Bước Chuẩn bị
+    - Cài đặt module SpeechRecognition: pip install speechrecognition
+    => Module thực hiện việc nhận dạng giọng nói và chuyển đổi thành văn bản
+    - Cài đặc module PyAudio: pip install pyaudio
+    => Module chấp nhận định dạng mặc định của tệp audio từ microphone
+    Ubuntu: Nếu khi cài bị error về portaudio thì giải quyết bằng cài: sudo apt-get install portaudio19-dev python-pyaudio
+    MacOS: https://programmerah.com/mac-pyaudio-installation-fatal-error-portaudio-h-file-not-found-33342/
+"""
+
+import speech_recognition as s2t
+import time
+
+
+recognizer = s2t.Recognizer()
+with s2t.Microphone() as micro:
+    print('Speak ...')
+    audio_input = recognizer.record(micro, duration=10)
+    print("Recording time:", time.strftime("%I:%M:%S"))
+
+    try:
+        # Tham số language sẽ giúp recognizer làm việc chuẩn hơn
+        text_output = recognizer.recognize_google(audio_input, language='vi-VN')
+        print("Text recognize from audio:", text_output)
+    except:
+        print("Can't process the audio input!")
+
+    print("Finished:",  time.strftime("%I:%M:%S"))
