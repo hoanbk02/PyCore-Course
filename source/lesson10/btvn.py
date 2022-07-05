93dea72032be0138e550429e84c5858f3d868b48
hoanbk02 <hoanbk02@gmail.com>
2022-07-05 18:30:39 +0700

    Lesson 10

---
+++ source/lesson10/btvn.py
@@ -0,0 +1,17 @@
+""" BÀI TẬP VỀ NHÀ:
+
+Bài 01. Mô phỏng lại trò chơi sau:
+- Người chơi tung 2 con xúc xắc 6 mặt, các mặt có số chấm là: 1, 2, 3, 4, 5, 6
+- Quan sát kết quả, tính tổng các mặt hướng lên sau khi 2 con xúc xắc đứng yên
+- Lần đầu tiên,
+    - Nếu tổng là 7 hoặc 11 => Người chơi THẮNG
+    - Nếu tổng là 2, 3 hoặc 12 => Người chơi THUA
+    - Nếu tổng là 4, 5, 6, 8, 9 hoặc 10 => Đây sẽ là ĐIỂM của người chơi, và sang vòng tiếp theo
+- Để giành được THẮNG, người chơi tiếp tục tung 2 con xúc xắc cho đến khi ra được tổng = ĐIỂM trong lần đầu tiên;
+nếu tung ra được tổng = 7 => Người chơi THUA
+
+Bài 02. Viết một chương trình để lấy ra tên class của một object?
+
+Bài 03. Hãy nghĩ ra một đối tượng trong thực tế, tìm thuộc tính và các hành vi của nó rồi xây dựng một class cho các đối tượng vừa nghĩ ra.
+
+"""
